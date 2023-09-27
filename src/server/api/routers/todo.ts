c5748762edc5caa01a2ed83c6fb472d1ce2d88a9
Jason Aviandre <jasonaviandre@gmail.com>
2023-09-27 21:45:31 +0800

    ✨ feat: add orderTodo server function

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -48,5 +48,55 @@ export const todoRouter = createTRPCRouter({
             },
             data: input.data
         })
+    }),
+    getTaskById: protectedProcedure.input(z.object({id: z.number()})).query(async ({ctx, input}) => {
+        return await ctx.db.todo.findFirst({
+            where: {id: input.id},
+            select: {
+                indexNumber: true,
+            }
+
+        })
+    }),
+    orderTodo: protectedProcedure.input(z.object({id: z.number(), prevTaskIndexNumber: z.number(), nextTaskIndexNumber: z.number()})).mutation(async ({ctx, input}) => {
+        const id = input.id;
+        const prevTaskIndexNumber = input.prevTaskIndexNumber;
+        const nextTaskIndexNumber = input.nextTaskIndexNumber
+        if (!prevTaskIndexNumber && !nextTaskIndexNumber) return;
+        let currTaskIndexNumber = 0;
+        if (!prevTaskIndexNumber) {
+            currTaskIndexNumber = nextTaskIndexNumber - 512
+        } else if (!nextTaskIndexNumber) {
+            currTaskIndexNumber = prevTaskIndexNumber + 512
+        } else  {
+            currTaskIndexNumber = Math.floor((prevTaskIndexNumber + nextTaskIndexNumber)/2)
+        }
+
+        try {
+            await ctx.db.todo.update({
+                where: {
+                    id,
+                },
+                data: {
+                    indexNumber: currTaskIndexNumber
+                }
+            })
+    
+            if (Math.abs(currTaskIndexNumber - prevTaskIndexNumber) <= 1 || Math.abs(currTaskIndexNumber - nextTaskIndexNumber) <= 1) {
+                await ctx.db.todo.updateMany({
+                    data: {
+                        indexNumber: {
+                            increment: 1024
+                        }
+                    }
+                })
+            }
+        } catch (error) {
+            
+        }
+
+        
+
+
     })
 })
